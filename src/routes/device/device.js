d4ef310123373a4966836a801ab06aa9ec8b790d
TBchip <bischoffthijs@gmail.com>
2021-12-25 13:22:07 +0100

    added name to getdevice

--- src/routes/device/device.js
+++ src/routes/device/device.js
@@ -5,13 +5,16 @@ const devices = require('../../devices/devices');
 let router = express.Router();
 
 router.get("/get", async (req, res) => {
-    let mac = req.query.mac;
+    let mac = req.body.mac;
     
     let error;
     let errorMsg;
     if(!mac) {
         error = 400;
         errorMsg = 'please supply {mac} as a http parameter';
+    }else if(!devices.deviceExists(mac)){
+        error = 400;
+        errorMsg = `no device with mac: ${mac}`;
     }
     
     if(error){
@@ -20,28 +23,32 @@ router.get("/get", async (req, res) => {
         res.end();
         return;
     }
-    
-    let deviceStats = devices.getDeviceStats(mac);
-    if(!deviceStats){
-        error = 404;
-        errorMsg = `device exists but could not find stats`;
-    }
-    
-    if(error){
-        res.status(error);
-        res.send(errorMsg);
-        res.end();
-        return;
+
+    let response = {
+        'name': devices.getDeviceName(mac),
+        'stats': devices.getDeviceStats(mac)
     }
     
     res.status(200);
-    res.send(deviceStats);
+    res.send(response);
+    res.end();
+});
+router.get("/getall", async (req, res) => {    
+    let deviceMacs = devices.getDeviceMacs();
+    let response = {};
+    for(let mac in deviceMacs){
+        response[mac]['name'] = devices.getDeviceName(mac);
+        response[mac]['stats'] = devices.getDeviceStats(mac);
+    }
+    
+    res.status(200);
+    res.send(response);
     res.end();
 });
 
 router.get("/switch", async (req, res) => {
-    let mac = req.query.mac;
-    let state = req.query.state;
+    let mac = req.body.mac;
+    let state = req.body.state;
     
     let error;
     let errorMsg;
@@ -91,8 +98,8 @@ router.get("/switch", async (req, res) => {
 });
 
 router.get("/startup", async (req, res) => {
-    let mac = req.query.mac;
-    let state = req.query.state;
+    let mac = req.body.mac;
+    let state = req.body.state;
     
     let error;
     let errorMsg;
